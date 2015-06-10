6b0132487dc436fa9bf5e22a48e248e1307c31a3
Ross Byrne <ross.byrne@outlook.ie>
2015-06-10 17:28:06 +0100

    Created Project
    Created the project and tested some UI stuff.

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour {
+
+	public Text outputText;
+	public Button displayButton;
+	public Button resetButton;
+
+	// displays text to output text
+	public void OutputText(){
+
+		outputText.text = "Hello World!";
+
+		displayButton.GetComponent<Button> ().gameObject.SetActive(false);
+		resetButton.GetComponent<Button> ().gameObject.SetActive(true);
+
+	} // OutputText()
+
+	public void ResetButton(){
+
+		outputText.text = "Output";
+
+		resetButton.GetComponent<Button> ().gameObject.SetActive(false);
+		displayButton.GetComponent<Button> ().gameObject.SetActive(true);
+
+	}
+}
