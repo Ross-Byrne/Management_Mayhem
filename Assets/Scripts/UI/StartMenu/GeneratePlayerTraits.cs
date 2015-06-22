b1a7b7945558b95b22ba636b925c90a7095190b7
Ross Byrne <ross.byrne@outlook.ie>
2015-06-22 12:17:34 +0100

    toggles for traits now generate via code
    toggles are generated depending on how many selectable traits there are
    to choose. Still need to do some configuring.

--- Assets/Scripts/UI/StartMenu/GeneratePlayerTraits.cs
+++ Assets/Scripts/UI/StartMenu/GeneratePlayerTraits.cs
@@ -7,14 +7,23 @@ using UnityEngine.UI;
 
 public class GeneratePlayerTraits : MonoBehaviour {
 
-	public Toggle traitToggle;
-	public int top = 80;
+	public Toggle traitTogglePrefab;
+	public float posY;
+	public float posX = 0;
 	public int arrayLength;
+
+	public int spacing = 25;
+	public int startingPoint = 0;
 
 	void Awake(){
 
 		// get the length of playerTraitSelect array
 		arrayLength = GameManager.gameManager.PlayerTraitsSelection.Length;
+
+		// Calculate starting position for trait toggles
+		// based on number of traits to choose from
+		// gets the aprox center of screen
+		posY = startingPoint + (arrayLength / 2) * spacing;		
 
 		// set up the traits
 		SetUpTraits ();
@@ -25,12 +34,23 @@ public class GeneratePlayerTraits : MonoBehaviour {
 
 		for (int i = 0; i < arrayLength; i++) {
 
-			Toggle toggle = (Toggle)Instantiate (traitToggle);
+			// set position of trait toggle
+			Vector3 pos = new Vector3(posX, posY, 0);
+
+			// instantiate toggle to set position
+			Toggle toggle = (Toggle)Instantiate (traitTogglePrefab, pos, Quaternion.identity);
+
+			// make toggle a child of "SelectCharacterTraitsMenu" GameObject
 			toggle.transform.SetParent (gameObject.transform, false);
-			//toggle.transform.position.Set(0,top-10, 0);
+
+			// set is to not be toggled
 			toggle.isOn = false;
+
+			// set the text on the toggle to the name of selectable trait
 			toggle.GetComponentInChildren<Text>().text = GameManager.gameManager.PlayerTraitsSelection[i];
 
+			// Decrement position of toggle by 25
+			posY -= spacing;
 		} // for
 
 	} // SetUpTraits()
