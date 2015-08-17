b602ed10922808499a51a7812971b6f77daf7159
Ross Byrne <ross.byrne@outlook.ie>
2015-08-17 17:27:56 +0100

    More Work On Finance Breakdown Panel
    Added a script to keep track of the UI elements on the finance breakdown
    panel. Added a method to hide or show the profits made from drugs
    depending on if your added to sell or make.

--- Assets/Scripts/Logic/BusinessFinances.cs
+++ Assets/Scripts/Logic/BusinessFinances.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class BusinessFinances : MonoBehaviour {
 
+	/*===================== GameObjects =====================================================================================*/
+
+	public GameObject financeBreakdownPanel;
+
+
 	/*===================== UI Elements =====================================================================================*/
 
 
@@ -21,5 +26,41 @@ public class BusinessFinances : MonoBehaviour {
 		// Select Maintenance as the default selected menu
 		gameObject.GetComponent<BusinessFinancesMenuControl> ().maintenanceButtonControl ();
 
+		// Check to see if can show drug related fields
+		CheckIfSellingOrMakingDrugs ();
 	} // SetUpMenu()
+
+
+	/*===================== CheckIfSellingOrMakingDrugs() =====================================================================================*/
+	
+	// checks to see if player is making or selling drugs
+	public void CheckIfSellingOrMakingDrugs(){
+		
+		if (GameManager.gameManager.CanStartMakingDrugs) { // if has drug lab
+
+			// show profits from selling drugs
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().dealerProfits.SetActive (true);
+
+			// show profits from drug lab
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().labProfits.SetActive (true);
+
+		} else if (GameManager.gameManager.CanStartSellingDrugs) { // if dont have lab but selling
+
+			// show profits from selling drugs
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().dealerProfits.SetActive (true);
+			
+			// hide profits from drug lab
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().labProfits.SetActive (false);
+
+		} else { // if not selling or doesnt have lab
+
+			// hide profits from selling drugs
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().dealerProfits.SetActive (false);
+			
+			// hide profits from drug lab
+			financeBreakdownPanel.GetComponent<FinanceBreakdownPanel> ().labProfits.SetActive (false);
+
+		}// if
+		
+	} // CheckIfSellingOrMakingDrugs()
 } // Class
--- Assets/Scripts/UI/MainGame/FinanceBreakdownPanel.cs
+++ Assets/Scripts/UI/MainGame/FinanceBreakdownPanel.cs
@@ -6,7 +6,29 @@ using UnityEngine.UI;
 
 public class FinanceBreakdownPanel : MonoBehaviour {
 
+	/*===================== GameObjects =====================================================================================*/
+
+	// Keeping track of those to hide them if not unlocked
+	public GameObject dealerProfits;
+	public GameObject labProfits;
+
 	/*===================== UI Elements =====================================================================================*/
+
+	// Text
+
+	public Text productionIncomeText;
+	public Text contractIncomeText;
+	public Text otherIncomeText;
+	public Text totalIncomeText;
+
+	public Text productionCostsText;
+	public Text employeeCostsText;
+	public Text maintenanceCostsText;
+	public Text totalCostsText;
+
+	public Text dealerProfitsText;
+	public Text labProfitsText;
+	public Text totalProfitsText;
 
 
 } // class
