15a1de77e35f557efc14fcafae6a27269cd31be1
Ross Byrne <ross.byrne@outlook.ie>
2015-09-14 10:57:37 +0100

    Working on Products generation

--- Assets/Scripts/Logic/Business/GenerateProducts.cs
+++ Assets/Scripts/Logic/Business/GenerateProducts.cs
@@ -7,35 +7,36 @@ public class GenerateProducts : MonoBehaviour {
 
     /*===================== Variables =====================================================================================*/
 
-    public Product[] products;
     public int numOfProducts;
     public string[] names = {"Micro Chips", "Circuit Boards", "Memory Modules", "Hard Drives", "Optical Drives", "Video Cards" };
     public int[] resourceCost = { 2, 3, 6, 7, 9, 12 };
     public int[] powerUsage = { 5, 5, 5, 5, 5, 10};
     public float[] profit = { 4f, 5.50f, 7f, 9f, 12f, 15f };
 
+    // the cost of each resource unit
+    public float costOfResourceUnits = 5f;
+
     /*===================== Methods =====================================================================================*/
 
-    /*===================== Awake() =====================================================================================*/
+    /*===================== GetNumberOfProducts() =====================================================================================*/
 
-    void Awake() {
+    public int GetNumberOfProducts() {
 
-        // set default number
         numOfProducts = 0;
 
         // get the number of products, based on number of names
         numOfProducts = names.Length;
 
-        // initialise array of products
-        products = new Product[numOfProducts];
-    
-    } // Awake()
+        // return number of products
+        return numOfProducts;
+
+    } // GetNumberOfProducts()
 
 
     /*===================== SetUpProducts() =====================================================================================*/
 
     // sets up the product values
-    public void SetUpProducts() {
+    public void SetUpProducts(Product[] products) {
 
         // if there aren't enough values to populate products
         if(resourceCost.Length != numOfProducts ||
@@ -50,7 +51,20 @@ public class GenerateProducts : MonoBehaviour {
         // populate product values
         for(int i = 0; i < numOfProducts; i++) {
 
+            // give products their number
+            products[i].Number = i + 1;
+            
+            // give products their names
+            products[i].Name = names[i];
 
+            // give products thier resource cost
+            products[i].ResourceCost = resourceCost[i];
+
+            // give products their power usage
+            products[i].PowerUsage = powerUsage[i];
+
+            // calculate products market value
+            products[i].MarketValue = ((resourceCost[i] * costOfResourceUnits) + profit[i]);
 
         } // for
 
